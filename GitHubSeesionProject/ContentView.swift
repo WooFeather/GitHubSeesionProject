c034b6d6e26536d4bbe225986f84b961fc054dcc
woboneyo <bluewing1004@gmail.com>
2024-04-18 20:00:40 +0900

    Initial Commit

---
+++ GitHubSeesionProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHubSeesionProject
+//
+//  Created by 조우현 on 4/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHubSeesionProject/GitHubSeesionProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubSeesionProjectApp.swift
+//  GitHubSeesionProject
+//
+//  Created by 조우현 on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubSeesionProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
